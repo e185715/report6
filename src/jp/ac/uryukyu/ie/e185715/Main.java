c494504a692f36997362245df380504f0bd13806
Shinto Higa <e185715@ie.u-ryukyu.ac.jp>
2019-01-17 01:49:09 +0900

    report6 2st commit  draw()実装済み

---
+++ src/jp/ac/uryukyu/ie/e185715/Main.java
@@ -0,0 +1,13 @@
+package jp.ac.uryukyu.ie.e185715;
+
+public class Main {
+    public static void main(String[] args){
+        Solver sol = new Solver();
+        
+        for(int i=0;i<11;i++)
+            sol.draw();
+            System.out.println(sol.p1_hands);
+
+
+    }
+}
--- src/jp/ac/uryukyu/ie/e185715/Solver.java
+++ src/jp/ac/uryukyu/ie/e185715/Solver.java
@@ -9,35 +9,49 @@ import java.util.Scanner;
 class Solver{
     String[] cards = new String[] {"1","2","3","4","5","6","7","8","9","10","11"};
     List<String> deck = new ArrayList<String>(Arrays.asList(cards));
-    List<String> p1_hands,enemy_hands;
-    boolean turn,battle;
-    int num,damage = 0;
-    String[] action = {"アイテム","カードを引く","これでしょうぶ！"};
+    String[] itemList = {"ドロー1","ドロー2","ドロー3","ドロー4","ドロー5","ドロー6","ドロー7","ドロー8","ドロー9","ドロー10","ドロー11","シールド","リセット","リムーブ","ダメージUP","ゴール24","ゴール27"};
+    List<String> item = new ArrayList<String>(Arrays.asList(itemList));
+    List<String> p1_hands= new ArrayList<>(),enemy_hands = new ArrayList<>();
+    boolean turn=true,battle=true;
+    int num,goal,damage;
     List<String> p1_item,enemy_item;
+    Scanner in = new Scanner(System.in);
 
     void initialize(){//山札手札の初期化
         deck = new ArrayList<String>(Arrays.asList(cards));
         p1_hands.clear();
         enemy_hands.clear();
+        System.out.println(p1_hands);
         damage += 1;
+        goal = 21;
+        for (int i=0;i<2;i++){
+            turn = true;
+            draw();
+            turn = false;
+            draw();
+        }
+    }
+
+    List<String> handring(){//引いたカードを、引いた人の手札に加えさせるためのboolean操作
+        if (this.turn == true){
+            return this.p1_hands;
+        }else{
+            return this.enemy_hands;
+        }
     }
 
     void draw(){//山札からカードを1枚引く
-        num = (int)Math.random()*deck.size();
-        if(this.turn == true){
-            p1_hands.add(String.valueOf(num));
-        } else{
-            enemy_hands.add(String.valueOf(num));
-        }
-        deck.remove(num);
+        num = (int)(Math.random()*deck.size());
+        p1_hands.add(deck.get(num));
+        deck.remove(deck.get(num));
     }
 
-    void item(){
-        System.out.println(p1_item);
-        
-
+    void getItem(){//お互いにアイテムをランダムでひとつ入手する
+        num = (int)Math.random()*item.size();
+        p1_item.add(item.get(num));
+        num = (int)Math.random()*item.size();
+        enemy_item.add(item.get(num));
     }
-
 
 
 }
